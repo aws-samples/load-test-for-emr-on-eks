2343ec35f4401e15aebb7b84c94fc2e46b62a9ec
meloyang <meloyang@amazon.com>
2026-03-24 00:47:39 +1100

    fix SPARK_JOB_NS_NUM issue

--- locust-provision.sh
+++ locust-provision.sh
@@ -133,7 +133,7 @@ sed -i='' 's|${ECR_URL}|'$ECR_URL'|g' examples/load-test-on-eks-${CLUSTER_NAME}.
 sed -i='' 's|${REGION}|'$AWS_REGION'|g' examples/load-test-on-eks-${CLUSTER_NAME}.yaml
 sed -i='' 's|${JOB_SCRIPT_NAME}|'$JOB_SCRIPT_NAME'|g' examples/load-test-on-eks-${CLUSTER_NAME}.yaml
 sed -i='' 's|${EMR_IMAGE_VERSION}|'$EMR_IMAGE_VERSION'|g' examples/load-test-on-eks-${CLUSTER_NAME}.yaml
-sed -i='' 's|${SPARK_JOB_NS_NUM}|'$SPARK_JOB_NS_NUM'|g' examples/load-test-on-eks-${CLUSTER_NAME}.yaml
+sed -i='' 's|${SPARK_JOB_NS_NUM}|"'$SPARK_JOB_NS_NUM'"|g' examples/load-test-on-eks-${CLUSTER_NAME}.yaml
 rm examples/*=
 
 echo "=============================================================="
